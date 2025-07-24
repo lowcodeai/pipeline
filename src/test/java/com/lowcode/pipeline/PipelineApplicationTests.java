f5a07bfe666ae77802bdb37e64de4e408d8db61c
Hyacinth Ali <hyacinth.ali@mail.mcgill.ca>
2025-07-24 13:31:59 -0400

    Initial commit

---
+++ src/test/java/com/lowcode/pipeline/PipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lowcode.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
